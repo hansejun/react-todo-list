f893a3a73a6cc4270d39fe7a0f6fdfee39cfd584
hansejun <tpwnszja2@gmail.com>
2022-04-04 16:06:23 +0900

    ㅕㅔ

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { useForm } from "react-hook-form";
 import styled from "styled-components";
 
@@ -7,60 +6,19 @@ const Form = styled.form`
   flex-direction: column;
 `;
 interface IForm {
-  email: string;
-  password: string;
-  password2: string;
-  name: string;
-  extraError?: string;
+  toDo: string;
 }
 
 function TodoList() {
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-    setError,
-  } = useForm<IForm>();
-
-  const onValid = (data: IForm) => {
-    if (data.password === data.password2) {
-      setError("password2", { message: "Passwords are not same" });
-    }
-  };
-
+  const { register, handleSubmit } = useForm<IForm>();
+  const onValid = (data: IForm) => console.log("Valid", data.toDo);
   return (
     <div>
       <Form onSubmit={handleSubmit(onValid)}>
         <input
-          {...register("email", {
-            required: "Check your name",
-            pattern: {
-              value: /^[A-Za-z0-9._%+-]+@naver.com$/,
-              message: "Only naver.com",
-            },
-          })}
-          type="text"
-          placeholder="Email"
+          {...register("toDo", { required: true })}
+          placeholder="Write a to do"
         />
-        <span>{errors?.email?.message}</span>
-        <input
-          {...register("password", { required: "Check your password" })}
-          type="password"
-          placeholder="Password"
-        />
-        <span>{errors?.password?.message}</span>
-        <input
-          {...register("password2", { required: "Check your password" })}
-          type="password"
-          placeholder="Check your password"
-        />
-        <span>{errors?.password2?.message}</span>
-        <input
-          {...register("name", { required: "Check your name" })}
-          type="text"
-          placeholder="Name"
-        />
-        <span>{errors?.name?.message}</span>
         <button>Create</button>
       </Form>
     </div>
