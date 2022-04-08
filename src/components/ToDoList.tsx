c9596a5c5231ec3a56e5d86ed3c1b718b026553f
hansejun <tpwnszja2@gmail.com>
2022-04-08 19:23:18 +0900

    Update ToDoList.tsx

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,12 +1,43 @@
-import { IToDo } from "../atoms";
+import React from "react";
+import { useSetRecoilState } from "recoil";
+import { IToDo, toDoAtom } from "../atoms";
 
-function ToDoList({ text }: IToDo) {
+function ToDoList({ text, category, id }: IToDo) {
+  const setToDos = useSetRecoilState(toDoAtom);
+  const onClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    const {
+      currentTarget: { name },
+    } = event;
+
+    setToDos((preTodo) => {
+      const targetIndex = preTodo.findIndex((todo) => todo.id === id);
+      const newTodo = { text, id, category: name as any };
+      return [
+        ...preTodo.slice(0, targetIndex),
+        newTodo,
+        ...preTodo.slice(targetIndex + 1),
+      ];
+    });
+  };
+
   return (
     <li>
       <span>{text}</span>
-      <button>To Do</button>
-      <button>DOING</button>
-      <button>DONE</button>
+      {category !== "TO_DO" && (
+        <button name="TO_DO" onClick={onClick}>
+          TO DO
+        </button>
+      )}
+      {category !== "DOING" && (
+        <button name="DOING" onClick={onClick}>
+          DOING
+        </button>
+      )}
+      {category !== "DONE" && (
+        <button name="DONE" onClick={onClick}>
+          DONE
+        </button>
+      )}
     </li>
   );
 }
